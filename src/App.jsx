251d4c48bdfeed6e82afd06f3e776c35a7eb7955
pranay7505 <pranay7505@gmail.com>
2025-03-23 15:22:06 +0530

    no

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,16 @@ import Navbar from './components/Navbar'
 import Skillset from './components/Skillset'
 
 function App() {
+  
+
   return (
     <>
-      <Navbar />
-      <section id="home">
-        <h1>Welcome to Home</h1>
-      </section>
-      <section id="projects">
-        <h1>My Projects</h1>
-      </section>
-      <section id="about">
-        <h1>About Me</h1>
-      </section>
-      <section id="contact">
-        <h1>Contact Me</h1>
-      </section>
+     <Navbar></Navbar>
+     <Aboutme></Aboutme>
+     <Skillset></Skillset>
+     <ContactMe></ContactMe>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
